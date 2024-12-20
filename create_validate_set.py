684d5d2205c5feb626ac5914af4b5aedcc399eff
kannlight <kanlight28@gmail.com>
2024-12-20 19:45:07 +0900

    人手評価用のjson生成スクリプトを作成

---
+++ create_validate_set.py
@@ -0,0 +1,21 @@
+# アノテーション精度の人手評価用に、設問となるjsonファイルを作成
+import os
+import json
+import random
+
+def create_set(num_talk):
+    all_talk = []
+    for filename in os.listdir('dataset2'):
+        data = {}
+        with open('dataset2/'+filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        if 'data' in data:
+            for talk in data['data']:
+                all_talk.append(talk)
+    random.shuffle(all_talk)
+    set = all_talk[:num_talk]
+    with open('set_to_validate', 'w') as f:
+        json.dump({'data':set}, f, indent=4,ensure_ascii=False)
+
+if __name__ == "__main__":
+    create_set(100)
