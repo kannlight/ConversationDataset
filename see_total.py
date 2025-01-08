20e6860b44cf2ac20aa2db8be3cd1c01fbae5fba
user media <kanlight28@gmail.com>
2025-01-08 23:16:11 +0900

    ラベル集計機能を追加

--- see_total.py
+++ see_total.py
@@ -3,29 +3,53 @@ import statistics
 import json
 import math
 import sys
+import numpy as np
+from pathlib import Path
 
 creating_data_dir = 'creating_data'
 poor_data_dir = 'poor_data'
 
-def print_statics(dir):
+def print_statics(path):
     data_num_info = []
-    for file in os.listdir(dir):
-        with open(dir+'/'+file, 'r', encoding='utf-8') as f:
-            data_num_info.append(len(json.load(f)['data']))
+    if Path(path).is_dir():
+        for file in os.listdir(path):
+            with open(path+'/'+file, 'r', encoding='utf-8') as f:
+                data_num_info.append(len(json.load(f)['data']))
+        print(path)
+        num_recp = len(data_num_info)
+        print('  num of recipients: {}'.format(num_recp))
+        num_data = sum(data_num_info)
+        print('  sum: {}'.format(num_data))
+        print('  max: {}'.format(max(data_num_info)))
+        print('  min: {}'.format(min(data_num_info)))
+        ave = num_data / num_recp
+        print('  ave: {}'.format(ave))
+        print('  med: {}'.format(statistics.median(data_num_info)))
+        pvar = statistics.pvariance(data_num_info, ave)
+        print('  pvar: {}'.format(pvar))
+        print('  pstd: {}'.format(math.sqrt(pvar)))
+    else:
+        with open(path, 'r', encoding='utf-8') as f:
+            num_data = len(json.load(f)['data'])
+            print(path)
+            print('  sum: {}'.format(num_data))
 
-    print(dir)
-    num_recp = len(data_num_info)
-    print('  num of recipients: {}'.format(num_recp))
-    num_data = sum(data_num_info)
-    print('  sum: {}'.format(num_data))
-    print('  max: {}'.format(max(data_num_info)))
-    print('  min: {}'.format(min(data_num_info)))
-    ave = num_data / num_recp
-    print('  ave: {}'.format(ave))
-    print('  med: {}'.format(statistics.median(data_num_info)))
-    pvar = statistics.pvariance(data_num_info, ave)
-    print('  pvar: {}'.format(pvar))
-    print('  pstd: {}'.format(math.sqrt(pvar)))
+def label_statistics(dir):
+    sum_label = np.zeros(8)
+
+    for file in os.listdir(dir):
+        data = {}
+        with open(dir+'/'+file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        
+        if 'data' in data:
+            for talk in data['data']:
+                sum_label = sum_label + np.array(talk['label'])
+    print('label sum:')
+    print(sum_label)
+    print('data num:')
+    print(sum_label.sum())
 
 if __name__ == "__main__":
     print_statics(sys.argv[1])
+    # label_statistics(sys.argv[1])
